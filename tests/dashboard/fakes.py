18dfd785b987ee9a3fa8f3153ea63ae7e55a7ffc
Simon Heybrock <simon.heybrock@ess.eu>
2025-11-24 08:01:41 +0000

    Remove empty file

--- tests/dashboard/fakes.py
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: BSD-3-Clause
-# Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
-"""Shared test fakes for dashboard tests."""
-
-# This file previously contained FakeJobOrchestrator, which has been removed.
-# Dashboard tests now use the real JobOrchestrator with faked dependencies.
-# See tests/dashboard/conftest.py for shared fixtures.
