5df8f27a16c53c26c25bac298c1e8aa865411a6a
Johannes Kasimir <johanneskasimir@gmail.com>
2026-04-30 10:39:20 +0200

    fix: accumulation point

--- src/ess/livedata/config/instruments/estia/factories.py
+++ src/ess/livedata/config/instruments/estia/factories.py
@@ -25,6 +25,7 @@ def setup_factories(instrument: Instrument) -> None:
     from ess.reduce.nexus.types import NeXusData
     from ess.reflectometry.types import (
         BeamDivergenceLimits,
+        CoordTransformGraph,
         Filename,
         LookupTableFilename,
         LookupTableRelativeErrorThreshold,
@@ -44,35 +45,6 @@ def setup_factories(instrument: Instrument) -> None:
     from ess.livedata.handlers.monitor_workflow_specs import TOAOnlyMonitorDataParams
     from ess.livedata.handlers.stream_processor_workflow import StreamProcessorWorkflow
 
-    IntensityWavelength = NewType('IntensityWavelength', sc.DataArray)
-    IntensityQ = NewType('IntensityQ', sc.DataArray)
-    IntensityThetaWavelength = NewType('IntensityThetaWavelength', sc.DataArray)
-
-    def _intensity_over_wavelength(
-        events: ReducibleData[SampleRun],
-        wavelength_bins: WavelengthBins,
-    ) -> IntensityWavelength:
-        return IntensityWavelength(
-            events.hist(wavelength=wavelength_bins, dim=events.dims)
-        )
-
-    def _intensity_over_q(
-        events: ReducibleData[SampleRun],
-        q_bins: QBins,
-    ) -> IntensityQ:
-        return IntensityQ(events.hist(Q=q_bins, dim=events.dims))
-
-    def _intensity_over_theta_wavelength(
-        events: ReducibleData[SampleRun],
-        wavelength_bins: WavelengthBins,
-        theta_bins: ThetaBins[SampleRun],
-    ) -> IntensityThetaWavelength:
-        return IntensityThetaWavelength(
-            events.hist(
-                theta=theta_bins, wavelength=wavelength_bins, dim=events.dims
-            ).transpose(['theta', 'wavelength'])
-        )
-
     @specs.monitor_handle.attach_factory()
     def _monitor_workflow_factory(source_name: str, params: TOAOnlyMonitorDataParams):
         return create_monitor_workflow(
@@ -88,9 +60,6 @@ def setup_factories(instrument: Instrument) -> None:
         params: specs.EstiaLiveDiagnosticsParams,
     ) -> StreamProcessorWorkflow:
         wf = EstiaWorkflow()
-        wf.insert(_intensity_over_wavelength)
-        wf.insert(_intensity_over_q)
-        wf.insert(_intensity_over_theta_wavelength)
         wf[Filename[SampleRun]] = get_nexus_geometry_filename('estia')
         wf[NeXusDetectorName] = source_name
         wf[LookupTableFilename] = estia_data.estia_wavelength_lookup_table()
@@ -98,12 +67,51 @@ def setup_factories(instrument: Instrument) -> None:
         wf[WavelengthBins] = params.wavelength_edges.get_edges()
         wf[QBins] = params.q_edges.get_edges()
         wf[ThetaBins[SampleRun]] = params.theta_edges.get_edges()
+
         wf[YIndexLimits] = (sc.scalar(0), sc.scalar(63))
         wf[ZIndexLimits] = (sc.scalar(0), sc.scalar(1535))
         wf[BeamDivergenceLimits] = (
             sc.scalar(-0.75, unit='deg'),
             sc.scalar(0.75, unit='deg'),
         )
+
+        IntensityThetaWavelength = NewType('IntensityThetaWavelength', sc.DataArray)
+        IntensityWavelength = NewType('IntensityWavelength', sc.DataArray)
+        IntensityQ = NewType('IntensityQ', sc.DataArray)
+
+        def _intensity_over_theta_wavelength(
+            events: ReducibleData[SampleRun],
+            wavelength_bins: WavelengthBins,
+            theta_bins: ThetaBins[SampleRun],
+        ) -> IntensityThetaWavelength:
+            return IntensityThetaWavelength(
+                events.hist(
+                    theta=theta_bins, wavelength=wavelength_bins, dim=events.dims
+                ).transpose(['theta', 'wavelength'])
+            )
+
+        def _intensity_over_q(
+            intensity: IntensityThetaWavelength,
+            q_bins: QBins[SampleRun],
+            graph: CoordTransformGraph[SampleRun],
+        ):
+            return (
+                intensity.assign_coords(
+                    wavelength=sc.midpoints(intensity.coords['wavelength']),
+                    theta=sc.midpoints(intensity.coords['theta']),
+                )
+                .transform_coords('Q', graph=graph)
+                .hist(Q=q_bins)
+            )
+
+        def _intensity_over_wavelength(
+            intensity: IntensityThetaWavelength,
+        ):
+            return intensity.sum('theta')
+
+        wf.insert(_intensity_over_wavelength)
+        wf.insert(_intensity_over_q)
+        wf.insert(_intensity_over_theta_wavelength)
 
         target_keys: dict[str, sciline.typing.Key] = {
             'i_of_wavelength': IntensityWavelength,
@@ -115,5 +123,5 @@ def setup_factories(instrument: Instrument) -> None:
             wf,
             dynamic_keys={source_name: NeXusData[NXdetector, SampleRun]},
             target_keys=target_keys,
-            accumulators=(ReducibleData[SampleRun],),
+            accumulators=(IntensityThetaWavelength,),
         )
